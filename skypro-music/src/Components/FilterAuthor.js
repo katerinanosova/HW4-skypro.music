6232de5e8cc4ff5f947cf0b5d7114ceefd35e52a
katerinanosova <katerinanosova@gmail.com>
2023-08-24 17:33:21 +0200

    Added filters

---
+++ skypro-music/src/Components/FilterAuthor.js
@@ -0,0 +1,15 @@
+export default function Filter ({ type, filterName, isActive, onShow, onHide }) {
+    
+    const filterList = filterName.map((item, index) =>
+        // eslint-disable-next-line react/no-array-index-key
+        <option className="filter__item" key={index}>{item}</option>)
+
+    return (
+        // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+        <div onClick={isActive ? onHide : onShow} type='button' className="filter__button button-author _btn-text">
+            {type}
+            {isActive ? <div className="filter__option">
+            {filterList}
+            </div> : ''}
+        </div>)  
+}
