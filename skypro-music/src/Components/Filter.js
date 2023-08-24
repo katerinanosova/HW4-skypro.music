b21f70ddae94d609b7549e69ae1b34a9477acc5f
katerinanosova <katerinanosova@gmail.com>
2023-08-24 17:54:49 +0200

    upd css

--- skypro-music/src/Components/Filter.js
+++ skypro-music/src/Components/Filter.js
@@ -6,7 +6,7 @@ export default function Filter ({ type, filterName, isActive, onShow, onHide }) 
 
     return (
         // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
-        <div onClick={isActive ? onHide : onShow} type='button' className="filter__button button-author _btn-text">
+        <div onClick={isActive ? onHide : onShow} type='button' className={isActive ? "filter__button filter__button_active button-author" : "filter__button button-author _btn-text"}>
             {type}
             {isActive ? <div className="filter__option">
             {filterList}
