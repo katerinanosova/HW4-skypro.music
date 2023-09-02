2fa5bc5cb99f3154227f7d99951e8338a9ea039c
katerinanosova <katerinanosova@gmail.com>
2023-09-02 17:56:26 +0200

    filter styled upd

--- skypro-music/src/Components/Tracklist/Filter.js
+++ skypro-music/src/Components/Tracklist/Filter.js
@@ -1,68 +1,19 @@
-import { styled } from 'styled-components';
-
-
-const StyledFilterItem = styled.option`
-&:hover {
-    color: #B672FF;
-    text-decoration: underline;
-  }
-`
-
-const StyledFilterOption = styled.div`
-width: 248px;
-background-color: #313131;
-color: #ffffff;
-border-radius: 12px;
-box-sizing: border-box;
-padding: 34px;
-display: flex;
-flex-direction: column;
-gap: 24px;
-align-items: flex-start;
-position: relative;
-left: -20px;
-bottom: -10px;
-font-size: 20px;
-line-height: 24px;
-`
-
-const StyledFilterButton = styled.div`
-font-style: normal;
-font-weight: 400;
-font-size: 16px;
-line-height: 24px;
-border: 1px solid #ffffff;
-border-radius: 60px;
-padding: 6px 20px;
-text-align: center;
-color: ${({ $isActive }) => ($isActive ? '#9A48F1' : '#ffffff')};
-appearance: none;
-width: 144px;
-height: 39px;
-background-color: #181818;
-&:not(:last-child) {
-    margin-right: 10px;
-  }
-`
-
-
-
-// const Styled = styled.div``
+import * as S from './Filter.styled';
 
 export default function Filter ({ type, filterName, isActive, onShow, onHide }) {
     
     const filterList = filterName.map((item, index) =>
         // eslint-disable-next-line react/no-array-index-key
-        <StyledFilterItem key={index}>{item}</StyledFilterItem>)
+        <S.FilterItem key={index}>{item}</S.FilterItem>)
 
     return (
         // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
-        <StyledFilterButton $isActive={isActive} className='_btn-text' onClick={isActive ? onHide : onShow} type='button'>
+        <S.FilterButton $isActive={isActive} className='_btn-text' onClick={isActive ? onHide : onShow} type='button'>
             {type}
-            {isActive ? <StyledFilterOption>
+            {isActive ? <S.FilterOption>
             {filterList}
-            </StyledFilterOption> : ''}
-        </StyledFilterButton>)  
+            </S.FilterOption> : ''}
+        </S.FilterButton>)  
 }
 
   /* className={isActive ? "filter__button filter__button_active button-author" : "filter__button button-author _btn-text"} */
