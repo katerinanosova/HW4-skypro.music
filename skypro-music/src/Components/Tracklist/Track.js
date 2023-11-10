c9a2358e6bf1236f9a293786623b815ccaebcf65
katerinanosova <katerinanosova@gmail.com>
2023-11-10 16:14:44 +0100

    fixed loading mockup

--- skypro-music/src/Components/Tracklist/Track.js
+++ skypro-music/src/Components/Tracklist/Track.js
@@ -9,60 +9,86 @@ import { setCurrentTrack } from '../../store/audioplayer/actions';
 
 export function GetTracks({ tracks, isLoading, getTracksError }) {
 
-
+  const defaultTracks =
+    [ 
+    {id: 1, name: "Guilt", author: "Nero", album: "Welcome Reality", duration_in_seconds: 284},
+    {id: 2, name: "Elektro", author: "Dynoro, Outwork, Mr. Gee", album: "Elektro", duration_in_seconds: 144 },
+    {id: 3, name: "Iam Fire", author: "Ali Bakgor", album: "Iam Fire", duration_in_seconds: 144},
+    {id: 4, name: "Non Stop", author: "Стоункат, Psychopath", album: "Non Stop", duration_in_seconds: 144},
+  ]
 
   const playingTrack = useSelector((store) => store.audioplayer.track)
   const isPlaying = useSelector((store) => store.audioplayer.playing)
   const dispatch = useDispatch()
+
+  if (isLoading) {
+    return (
+      defaultTracks.map(track => 
+        <S.PlaylistItem key={track.id}>
+        <S.PlaylistTrack >
+          <S.TrackTitle>
+            <div>
+              <S.TrackTitleImageLoading /> 
+                <S.TrackTitleSvg alt="music">
+                <use xlinkHref="img/icon/sprite.svg#icon-note"/>
+              </S.TrackTitleSvg> 
+            </div>
+            <S.TrackTitleTextLoading />
+          </S.TrackTitle>
+          <S.TrackAuthorLoading /> 
+          <S.TrackAlbum>
+            <S.TrackAlbumLinkLoading />
+          </S.TrackAlbum>         
+        </S.PlaylistTrack>
+      </S.PlaylistItem> 
+        )
+    )
+  }
+
+
  
   const trackList = tracks.map(track => 
         <S.PlaylistItem key={track.id}>
         <S.PlaylistTrack onClick={() => {dispatch(setCurrentTrack({ playlist: tracks, track: track }))}}>
           <S.TrackTitle>
-          <div>
-            {isLoading || getTracksError ? <S.TrackTitleImageLoading /> : 
+          <div> 
             <S.TrackTitleImage>
               {playingTrack && playingTrack.id === track.id ? 
               <S.TrackTitleDotSvg $isPlaying={isPlaying} alt="music" />
               : <S.TrackTitleSvg alt="music">
               <use xlinkHref="img/icon/sprite.svg#icon-note"/>
             </S.TrackTitleSvg> }
-           </S.TrackTitleImage>}
+           </S.TrackTitleImage>
           </div>
-          {isLoading || getTracksError ? <S.TrackTitleTextLoading /> : <S.TrackTitleText>
+          <S.TrackTitleText>
             <S.TrackTitleLink href="http://">
                 {track.name} 
                 <S.TrackTitleSpan>
                 {track.version ? track.version : ''}
                 </S.TrackTitleSpan>
             </S.TrackTitleLink>
-          </S.TrackTitleText> }
+          </S.TrackTitleText>
           </S.TrackTitle>
-          {isLoading || getTracksError
-          ? <S.TrackAuthorLoading /> 
-          : <S.TrackAuthor>
+          <S.TrackAuthor>
             <S.TrackAuthorLink href="http://">
             {track.author}
             </S.TrackAuthorLink>
-          </S.TrackAuthor> }
-          
+          </S.TrackAuthor>
           <S.TrackAlbum>
-            {isLoading || getTracksError
-            ? <S.TrackAlbumLinkLoading />
-            : <S.TrackAlbumLink href="http://">
+            <S.TrackAlbumLink href="http://">
               {track.album}
-              </S.TrackAlbumLink>}     
+              </S.TrackAlbumLink>     
           </S.TrackAlbum>
-          {isLoading || getTracksError ? '' : <S.TrackTime>
+          <S.TrackTime>
             <S.TrackTimeSvg alt="time">
               <use xlinkHref="img/icon/sprite.svg#icon-like" />
             </S.TrackTimeSvg>
             <S.TrackTimeText>{getTrackDuration(track.duration_in_seconds)}</S.TrackTimeText>
-          </S.TrackTime> }         
+          </S.TrackTime>         
         </S.PlaylistTrack>
       </S.PlaylistItem> 
         )
         return (
-          trackList
+          getTracksError || trackList
         );
 }
